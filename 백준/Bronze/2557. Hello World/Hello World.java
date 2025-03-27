08ac3f48c97ee7d09cd41e2c21eed08f21c2e329
김동훈 <70877444+ehdgnsl01@users.noreply.github.com>
2025-03-28 00:56:29 +0900

    [Bronze V] Title: Hello World, Time: 96 ms, Memory: 13900 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,5 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String args[]){
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        writer.write("Hello World!");
+        writer.flush();
     }
 }
