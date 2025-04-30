9edc907d7b951743a1d39638e694389742cae3a3
김동훈 <70877444+ehdgnsl01@users.noreply.github.com>
2025-04-30 13:35:09 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 772 ms, Memory: 43508 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        Map<String,Integer> map = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            String s=br.readLine();
+            if(s.length()<m) continue;
+            map.put(s, map.getOrDefault(s, 0)+1);
+        }
+        List<String> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                // 자주 등장하는 단어 순서대로 정렬
+                if (Integer.compare(map.get(o1), map.get(o2)) != 0) {
+                    return Integer.compare(map.get(o2), map.get(o1));
+                }
+                // 등장 횟수가 같으면 길이가 긴 단어가 먼저 오도록 정렬
+                if (o1.length() != o2.length()) {
+                    return o2.length() - o1.length();
+                }
+                // 등장 횟수와 길이가 같으면 사전 순으로 정렬
+                return o1.compareTo(o2);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for (String str : list) {
+            sb.append(str + "\n");
+        }
+
+        bw.write(sb.toString());
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
