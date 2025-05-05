ddf6cc5552327bd99bc9e678181d22a7890303ce
김동훈 <70877444+ehdgnsl01@users.noreply.github.com>
2025-05-05 23:19:41 +0900

    [Bronze II] Title: 피로도, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/22864. 피로도/피로도.java
+++ 백준/Bronze/22864. 피로도/피로도.java
@@ -1,31 +1,27 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private void solution() throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int sum = 0;
-        int aSum = 0;
-        for (int i = 0; i < 24; i++) {
-        	if (aSum+a <= m) {
-        		sum += b;
-        		aSum += a;
-        	} else {
-        		aSum -= c;
-        		if (aSum < 0)
-        			aSum = 0;
-        	}
-        }
-        System.out.print(sum);
-    }
-
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
-    }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int count = 0, p = 0, answer = 0;
+		while(count != 24) {
+			if(p + a <= m) {
+				answer += b;
+				p += a;
+			}
+			else {
+				p = p - c < 0 ? 0 : p - c;
+			}
+			count++;
+		}
+		System.out.print(answer);
+	}
 }
